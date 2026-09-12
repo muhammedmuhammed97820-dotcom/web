a3e027be9291167e32757f3110634ff9e244ed57
muhammedmuhammed97820-dotcom <muhammedmuhammed97820@gmail.com>
2026-09-12 11:39:05 +0300

    Wire redesigned library pages into Angular routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { BooksComponent } from './pages/books/books.component';
+import { CategoriesComponent } from './pages/categories/categories.component';
+import { AuthorsComponent } from './pages/authors/authors.component';
+import { AuthComponent } from './pages/auth/auth.component';
+import { AdminComponent } from './pages/admin/admin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'',component:HomeComponent,title:'المكتبة الإلكترونية'},
+  {path:'books',component:BooksComponent,title:'الكتب'},
+  {path:'categories',component:CategoriesComponent,title:'التصنيفات'},
+  {path:'authors',component:AuthorsComponent,title:'المؤلفون'},
+  {path:'auth',component:AuthComponent,title:'تسجيل الدخول'},
+  {path:'admin',component:AdminComponent,title:'لوحة الإدارة'},
+  {path:'**',redirectTo:''}
+];
