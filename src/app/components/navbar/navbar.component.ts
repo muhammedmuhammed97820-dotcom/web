aca5ab5ae19b51794eedfcdbbcdf12ac2da6c475
muhammedmuhammed97820-dotcom <muhammedmuhammed97820@gmail.com>
2026-09-12 11:55:45 +0300

    Upgrade navbar interactions

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,14 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-@Component({selector:'app-navbar',standalone:true,imports:[RouterLink,RouterLinkActive],templateUrl:'./navbar.component.html',styleUrl:'./navbar.component.scss'})
-export class NavbarComponent { @Output() customize=new EventEmitter<void>(); }
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+  @Output() customize = new EventEmitter<void>();
+  @Output() themeToggle = new EventEmitter<void>();
+}
