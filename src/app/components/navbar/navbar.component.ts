144d18ed5f49106aaa79725b62f58666ce70d0df
muhammedmuhammed97820-dotcom <muhammedmuhammed97820@gmail.com>
2026-09-12 11:37:41 +0300

    Add glass navigation component

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,4 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+@Component({selector:'app-navbar',standalone:true,imports:[RouterLink,RouterLinkActive],templateUrl:'./navbar.component.html',styleUrl:'./navbar.component.scss'})
+export class NavbarComponent { @Output() customize=new EventEmitter<void>(); }
